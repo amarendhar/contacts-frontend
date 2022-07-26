f15c08b5edffe99a5812b9d4825dec1d6f9da485
amarendhar <amarendhar.ganji@yahoo.com>
2022-07-26 10:38:22 +0400

    Restore apolloClient uri with localhost intead of ip-address

--- src/apolloClient.ts
+++ src/apolloClient.ts
@@ -4,7 +4,7 @@ const apolloClient = new ApolloClient({
   uri:
     process.env.NODE_ENV === "production"
       ? "replace-this-url-with-production-contacts-service-url"
-      : "http://192.168.0.101:8001/graphql",
+      : "http://localhost:8001/graphql",
   cache: new InMemoryCache({
     // To remove `__typename` from the graphql-query-response.
     addTypename: false,
